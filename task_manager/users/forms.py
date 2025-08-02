71521259e30de639a74b2438f6581ffc0ed7beb8
MyName <ivant4221@gmail.com>
2025-08-02 12:48:37 +0300

    fix user edit form

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -65,24 +65,24 @@ class LoginUserForm(AuthenticationForm):
 class UserEditForm(forms.ModelForm):
     first_name = forms.CharField(
         label="Имя",
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Имя'})
     )
     last_name = forms.CharField(
         label="Фамилия",
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'ПарФамилияоль'})
     )
     username = forms.CharField(
         label="Имя пользователя",
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Имя пользователя'})
     )
-    password = forms.CharField(
+    password1 = forms.CharField(
         label="Пароль",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'Пароль'}),
         required=True
     )
     password2 = forms.CharField(
-        label="Повтор пароля",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'Подтверждение пароля'}),
         required=True
     )
 
